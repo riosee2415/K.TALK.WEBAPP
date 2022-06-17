963352be11ec4eedd538db9f4b326a1f3039c07c
hmg11123 <=>
2022-06-17 17:57:49 +0900

    modify

--- front/reducers/normalNotice.js
+++ front/reducers/normalNotice.js
@@ -12,25 +12,65 @@ export const initialState = {
   normalNoticeListDone: false,
   normalNoticeListError: false,
 
-  normalNoticeListLoading: false, // 회원 일반게시판 가져오기
-  normalNoticeListDone: false,
-  normalNoticeListError: false,
+  normalNoticeAdminListLoading: false, // 관리자 일반게시판 가져오기
+  normalNoticeAdminListDone: false,
+  normalNoticeAdminListError: false,
 };
 
 export const NORMAL_NOTICE_LIST_REQUEST = "NORMAL_NOTICE_LIST_REQUEST";
 export const NORMAL_NOTICE_LIST_SUCCESS = "NORMAL_NOTICE_LIST_SUCCESS";
 export const NORMAL_NOTICE_LIST_FAILURE = "NORMAL_NOTICE_LIST_FAILURE";
 
+export const NORMAL_NOTICE_ADMIN_LIST_REQUEST =
+  "NORMAL_NOTICE_ADMIN_LIST_REQUEST";
+export const NORMAL_NOTICE_ADMIN_LIST_SUCCESS =
+  "NORMAL_NOTICE_ADMIN_LIST_SUCCESS";
+export const NORMAL_NOTICE_ADMIN_LIST_FAILURE =
+  "NORMAL_NOTICE_ADMIN_LIST_FAILURE";
+
 const reducer = (state = initialState, action) =>
   produce(state, (draft) => {
     switch (action.type) {
+      //////////////////////////////////////////////    회원 일반게시판 가져오기
       case NORMAL_NOTICE_LIST_REQUEST: {
+        draft.normalNoticeListLoading = true;
+        draft.normalNoticeListDone = false;
+        draft.normalNoticeListError = null;
         break;
       }
       case NORMAL_NOTICE_LIST_SUCCESS: {
+        draft.normalNoticeListLoading = false;
+        draft.normalNoticeListDone = true;
+        draft.normalNoticeListError = null;
+        draft.normalNoticeList = action.data.notice;
+        draft.normalNoticeLastPage = action.data.lastPage;
         break;
       }
       case NORMAL_NOTICE_LIST_FAILURE: {
+        draft.normalNoticeListLoading = false;
+        draft.normalNoticeListDone = false;
+        draft.normalNoticeListError = action.error;
+        break;
+      }
+
+      //////////////////////////////////////////////    관리자 일반게시판 가져오기
+      case NORMAL_NOTICE_LIST_REQUEST: {
+        draft.normalNoticeAdminListLoading = true;
+        draft.normalNoticeAdminListDone = false;
+        draft.normalNoticeAdminListError = null;
+        break;
+      }
+      case NORMAL_NOTICE_LIST_SUCCESS: {
+        draft.normalNoticeAdminListLoading = false;
+        draft.normalNoticeAdminListDone = true;
+        draft.normalNoticeAdminListError = null;
+        draft.normalNoticeAdminList = action.data.notice;
+        break;
+      }
+      case NORMAL_NOTICE_LIST_FAILURE: {
+        draft.normalNoticeAdminListLoading = false;
+        draft.normalNoticeAdminListDone = false;
+        draft.normalNoticeAdminListError = action.error;
         break;
       }
 
--- front/sagas/normalNotice.js
+++ front/sagas/normalNotice.js
@@ -1,23 +1,88 @@
 import { all, call, delay, fork, put, takeLatest } from "redux-saga/effects";
 import axios from "axios";
+import {
+  ///////////////////////////////// 회원 일반게시판 가져오기
+  NORMAL_NOTICE_LIST_REQUEST,
+  NORMAL_NOTICE_LIST_SUCCESS,
+  NORMAL_NOTICE_LIST_FAILURE,
+  ///////////////////////////////// 관리자 일반게시판 가져오기
+  NORMAL_NOTICE_ADMIN_LIST_REQUEST,
+  NORMAL_NOTICE_ADMIN_LIST_SUCCESS,
+  NORMAL_NOTICE_ADMIN_LIST_FAILURE,
+} from "../reducers/normalNotice";
 
 // ******************************************************************************************************************
 // SAGA AREA ********************************************************************************************************
 // ******************************************************************************************************************
 
+async function normalNoticeListAPI(data) {
+  return await axios.post(`/api/normalNotice/list`, data);
+}
+
+function* normalNoticeList(action) {
+  try {
+    const result = yield call(normalNoticeListAPI, action.data);
+
+    yield put({
+      type: NORMAL_NOTICE_LIST_SUCCESS,
+      data: result.data,
+    });
+  } catch (err) {
+    console.error(err);
+    yield put({
+      type: NORMAL_NOTICE_LIST_FAILURE,
+      error: err.response.data,
+    });
+  }
+}
+
+// ******************************************************************************************************************
+// ******************************************************************************************************************
+// ******************************************************************************************************************
+
+// ******************************************************************************************************************
+// SAGA AREA ********************************************************************************************************
+// ******************************************************************************************************************
+
+async function normalNoticeAdminListAPI(data) {
+  return await axios.post(`/api/normalNotice/admin/list`, data);
+}
+
+function* normalNoticeAdminList(action) {
+  try {
+    const result = yield call(normalNoticeAdminListAPI, action.data);
+
+    yield put({
+      type: NORMAL_NOTICE_ADMIN_LIST_SUCCESS,
+      data: result.data,
+    });
+  } catch (err) {
+    console.error(err);
+    yield put({
+      type: NORMAL_NOTICE_ADMIN_LIST_FAILURE,
+      error: err.response.data,
+    });
+  }
+}
+
 // ******************************************************************************************************************
 // ******************************************************************************************************************
 // ******************************************************************************************************************
 
 //////////////////////////////////////////////////////////////
-// function* watchMessageSenderList() {
-//     yield takeLatest(MESSAGE_SENDER_LIST_REQUEST, messageSenderList);
-//   }
+function* watchNormalNoticeList() {
+  yield takeLatest(NORMAL_NOTICE_LIST_REQUEST, normalNoticeList);
+}
+
+function* watchNormalNoticeAdminList() {
+  yield takeLatest(NORMAL_NOTICE_ADMIN_LIST_REQUEST, normalNoticeAdminList);
+}
 
 //////////////////////////////////////////////////////////////
 export default function* normalNoticeSaga() {
   yield all([
-    //   fork(watchMessageUserList),
+    fork(watchNormalNoticeList),
+    fork(watchNormalNoticeAdminList),
     //
   ]);
 }
