5356bb1e58d40ed10ef8fd9c947ba51859d1bf08
sts2003 <4leaf.sts@gmail.com>
2022-07-02 19:13:47 +0900

    modify

--- back/routers/normalNoticeRouter.js
+++ back/routers/normalNoticeRouter.js
@@ -44,7 +44,7 @@ const upload = multer({
       );
     },
   }),
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB
+  limits: { fileSize: 20 * 1024 * 1024 }, // 20MB
 });
 
 const router = express.Router();
