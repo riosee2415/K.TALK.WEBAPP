284d12624f2b33b458c4af891d112adc8929d76f
Song-jaehong <4leaf.SJH@gmail.com>
2022-08-04 15:19:54 +0900

    a

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -7,6 +7,7 @@ import Theme from "../components/Theme";
 import GlobalStyles from "../components/GlobalStyles";
 import wrapper from "../store/configureStore";
 import WidthProvider from "../components/WidthProvider";
+import Pixcel from "./pixcel";
 
 const Fourleaf = ({ Component }) => {
   return (
@@ -38,29 +39,9 @@ const Fourleaf = ({ Component }) => {
           }}
         ></script>
 
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `!function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '483937212685511');
-          fbq('track', 'PageView');`,
-          }}
-        ></script>
-
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style="display:none"
-            src="https://www.facebook.com/tr?id=483937212685511&ev=PageView&noscript=1"
-          />
-        </noscript>
+        <div>
+          <Pixcel name="FACEBOOK_PIXEL_1" />
+        </div>
       </Head>
       <Component />
     </ThemeProvider>
