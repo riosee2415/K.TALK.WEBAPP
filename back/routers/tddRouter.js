8da3b46ad7e8860531abcdfe0c09121033cc35e0
Song-jaehong <4leaf.SJH@gmail.com>
2022-10-25 01:11:43 +0900

    a

---
+++ back/routers/tddRouter.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const isAdminCheck = require("../middlewares/isAdminCheck");
+const isLoggedIn = require("../middlewares/isLoggedIn");
+
+const models = require("../models");
+
+const router = express.Router();
+
+///
+
+///
+
+module.exports = router;
